0ea40a885b8c45a04903ee1410f610b5d6779930
Nikita-2406 <89524208686@mail.ru>
2024-11-04 22:34:44 +0300

    3.0.0

--- src/js/app.js
+++ src/js/app.js
@@ -2,9 +2,41 @@ let btnsAdd = document.querySelectorAll(".block");
 let actualElem;
 let actualElemXY;
 let lastMouseElem;
-let new_elem_opasity;
 let newBlock;
 let mouseUpItem;
+let new_elem_opasity = createNewCard("213452345");
+new_elem_opasity.style.opacity = 0;
+new_elem_opasity.classList.add("unvis");
+
+const mouseDown = (e) => {
+  e.preventDefault();
+  
+  actualElem = e.target.closest('.card');
+  if (actualElem.querySelector(".card_delete") === e.target) {
+    actualElem.remove()
+  } else {newBlock = e.target.closest(".block");
+  new_elem_opasity.innerHTML = actualElem.innerHTML;
+  actualElemXY = [
+    e.clientX - actualElem.offsetLeft,
+    e.clientY - actualElem.offsetTop,
+  ];
+  newBlock.querySelector('.body--cards').append(new_elem_opasity)
+  mouseUpItem = newBlock.querySelector('.body--cards')
+  actualElem.classList.add("dragged");
+  document.documentElement.addEventListener("mouseup", onMouseUp);
+  document.documentElement.addEventListener("mousemove", onMouseOver);}
+  
+}
+
+function createNewCard(text) {
+  const elem = document.createElement("div");
+  elem.classList.add("card");
+  const content = `<div class="card_text">${text}</div>
+        <button class="card_delete">&#215;</button>`;
+
+  elem.insertAdjacentHTML("beforeend", content);
+  return elem
+}
 
 function toogleMove(elem1, elem2) {
   elem1.classList.toggle("unvisible");
@@ -14,9 +46,7 @@ function toogleMove(elem1, elem2) {
 function formOnSubmit(e, form, bodyCards, btn) {
   e.preventDefault();
   let textarea = form.querySelector("textarea");
-  const card = document.createElement("div");
-  card.classList.add("card");
-  card.textContent = textarea.value;
+  const card = createNewCard(textarea.value);
   bodyCards.append(card);
   toogleMove(btn, form);
   textarea.value = "";
@@ -27,7 +57,6 @@ for (const elem of btnsAdd) {
   let form = elem.querySelector(".add--card--form");
   let formBackBtn = elem.querySelector(".back--btn");
   let bodyCards = elem.querySelector(".body--cards");
-
   btn.addEventListener("click", () => {
     toogleMove(btn, form);
   });
@@ -41,19 +70,7 @@ for (const elem of btnsAdd) {
     formOnSubmit(e, form, bodyCards, btn);
   });
 
-  bodyCards.addEventListener("mousedown", (e) => {
-    e.preventDefault();
-    actualElem = e.target;
-    newBlock = e.target.closest(".block");
-    actualElemXY = [
-      e.clientX - actualElem.offsetLeft,
-      e.clientY - actualElem.offsetTop,
-    ];
-
-    actualElem.classList.add("dragged");
-    document.documentElement.addEventListener("mouseup", onMouseUp);
-    document.documentElement.addEventListener("mousemove", onMouseOver);
-  });
+  bodyCards.addEventListener("mousedown", mouseDown);
 }
 
 const onMouseOver = (e) => {
@@ -61,15 +78,20 @@ const onMouseOver = (e) => {
   actualElem.style.top = `${e.clientY - actualElemXY[1]}px`;
   newBlock = e.target.closest(".block");
 
+  if (e.target.classList.value == "card") {
+    mouseUpItem = e.target;
+    if (e.target.offsetHeight / 2 <= e.y - e.target.offsetTop) {
+      new_elem_opasity = e.target
+        .insertAdjacentElement('beforebegin', new_elem_opasity)
+    } else {
+      new_elem_opasity = e.target
+        .insertAdjacentElement('afterend', new_elem_opasity)
+    }
+    return
+    } 
+
   if (newBlock && newBlock !== lastMouseElem) {
-    new_elem_opasity ? new_elem_opasity.remove() : console.log("000");
-
-    new_elem_opasity = document.createElement("div");
-    new_elem_opasity.innerHTML = actualElem.innerHTML;
-    new_elem_opasity.style.opacity = 0;
-    new_elem_opasity.classList.add("card");
-    new_elem_opasity.classList.add("unvis");
-
+    mouseUpItem = newBlock.querySelector(".body--cards")
     new_elem_opasity = newBlock
       .querySelector(".body--cards")
       .appendChild(new_elem_opasity);
@@ -77,23 +99,23 @@ const onMouseOver = (e) => {
     lastMouseElem = newBlock;
   }
 
-  if (e.target.classList.value == "card") {
-    newBlock
-      .querySelector(".body--cards")
-      .insertBefore(new_elem_opasity, e.target);
-    mouseUpItem = e.target;
-  }
 };
 
 const onMouseUp = (e) => {
-  if (mouseUpItem.closest(".block") !== null) {
-    let newField = e.target.closest(".block").querySelector(".body--cards");
-    let elem = document.createElement("div");
-    elem.classList.add("card");
-    elem.innerHTML = actualElem.innerHTML;
-    newField.appendChild(elem);
+  if (mouseUpItem) {
+
+    const elem = createNewCard(actualElem.querySelector('.card_text').textContent)
+    
     if (mouseUpItem.classList.value == "card") {
-      newField.insertBefore(elem, mouseUpItem);
+
+      if (mouseUpItem.offsetHeight / 2 <= e.y - mouseUpItem.offsetTop) {
+        mouseUpItem.insertAdjacentElement('afterend', elem)
+      } else {
+        mouseUpItem.insertAdjacentElement('beforebegin', elem)
+      }
+
+    } else {
+      mouseUpItem.append(elem)
     }
 
     actualElem.remove();
@@ -107,7 +129,6 @@ const onMouseUp = (e) => {
 
   actualElem = undefined;
   lastMouseElem = undefined;
-  new_elem_opasity = undefined;
   document.documentElement.removeEventListener("mouseup", onMouseUp);
   document.documentElement.removeEventListener("mousemove", onMouseOver);
 };
@@ -117,7 +138,7 @@ window.addEventListener("beforeunload", () => {
   const blocks = document.querySelectorAll(".block");
   for (const elem of blocks) {
     const readyText = [];
-    for (const card of elem.querySelectorAll(".card")) {
+    for (const card of elem.querySelectorAll(".card_text")) {
       readyText.push(card.textContent);
     }
     blockData[elem.querySelector("h2").textContent] = readyText;
@@ -141,9 +162,9 @@ document.addEventListener("DOMContentLoaded", () => {
         .querySelector(`[name="${el}"]`)
         .querySelector(".body--cards");
       for (const value of formData[el]) {
-        const elem = document.createElement("div");
-        elem.classList.add("card");
-        elem.textContent = value;
+        const elem = createNewCard(value);
+        // elem.classList.add("card");
+        // elem.textContent = value;
         place.append(elem);
       }
     });
